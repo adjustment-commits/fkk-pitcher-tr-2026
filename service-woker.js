e642bda9475e545eabaeb214357c59deecfa5a32
adjustment-commits <adjustment.h.nishimoto@gmail.com>
2025-12-20 20:57:56 +0900

    Create service-woker.js

---
+++ service-woker.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = "fkk-pitcher-jan-v1";
+
+const CACHE_FILES = [
+"./",
+"./index.html",
+"./manifest.json",
+"./icons/icon-192.png",
+"./icons/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+event.waitUntil(
+caches.open(CACHE_NAME).then(cache => cache.addAll(CACHE_FILES))
+);
+});
+
+self.addEventListener("activate", event => {
+event.waitUntil(
+caches.keys().then(keys =>
+Promise.all(
+keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+)
+)
+);
+});
+
+self.addEventListener("fetch", event => {
+event.respondWith(
+caches.match(event.request).then(res => res || fetch(event.request))
+);
+});
