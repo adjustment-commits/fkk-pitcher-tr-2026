0b39cbb608ad7dc8cdf01370f174eb4b4180d2c9
adjustment-commits <adjustment.h.nishimoto@gmail.com>
2025-12-21 20:48:48 +0900

    Update service-woker.js

--- service-woker.js
+++ service-woker.js
@@ -1,31 +1,30 @@
-const CACHE_NAME = "fkk-pitcher-jan-v1";
+const CACHE_NAME = "fkk-pitcher-vol01-v1";
 
 const CACHE_FILES = [
 "./",
 "./index.html",
 "./manifest.json",
+"./service-worker.js",
 "./icons/icon-192.png",
 "./icons/icon-512.png"
 ];
 
-self.addEventListener("install", event => {
+self.addEventListener("install", (event) => {
 event.waitUntil(
-caches.open(CACHE_NAME).then(cache => cache.addAll(CACHE_FILES))
+caches.open(CACHE_NAME).then((cache) => cache.addAll(CACHE_FILES))
 );
 });
 
-self.addEventListener("activate", event => {
+self.addEventListener("activate", (event) => {
 event.waitUntil(
-caches.keys().then(keys =>
-Promise.all(
-keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
-)
+caches.keys().then((keys) =>
+Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
 )
 );
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch", (event) => {
 event.respondWith(
-caches.match(event.request).then(res => res || fetch(event.request))
+caches.match(event.request).then((res) => res || fetch(event.request))
 );
-});
+})
